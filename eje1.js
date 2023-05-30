e85705e9aba27b629f9fe60b34f3d86d45abb70e
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-05-30 12:37:00 -0600

    created definitions, rules, and first excercises in JS

---
+++ eje1.js
@@ -0,0 +1,10 @@
+console.clear(); //(works for cleaning)
+var num1 = 2;
+var num2 = 4;
+var num3 = 6;
+
+var sum = num1+num2+num3;
+console.log (sum);
+
+var firstName = "Quafi";
+console.log(firstName);
---
+++ eje2.js
@@ -0,0 +1,87 @@
+console.clear(); //this allows to show one result rather a various results fromn the first value
+
+var fistNum = 10;
+console.log(fistNum);
+
+var secondNum= 15;
+console.log(secondNum);
+
+//Camel Case (Recommended for JS)
+var firstNum;
+//Pascal Case
+var FirstNum;
+//Snake Case
+var first_Num;
+
+//Rules for naming JS variables
+/* 
+-Name can contain letters, digits, _ and $
+
+var mySubject1;
+math$;
+
+-Name cannot start with a digit
+
+var 1maths;
+
+-Name can start with an _ or $
+
+var _subjectmarks, $rankInClass;
+
+-Names are case Sensitive
+
+var x, X;
+
+-Names cannot be reserve keywords
+
+var var (unspected token; is wrong)
+
+
+
+
+EXERCISE
+
+Which of these variables have valid names and variables have invalid names? Give reason
+
+
+1. &alpha1  
+This is a valid name because we can used the sign of $ when starting the variable
+
+2. NUM1
+This is an invalid name because JS is a case sensitive this means we can't use only capital letters 
+
+3. DATE-5
+
+This is an invalid name because only has a capital letters and has a sign - 
+
+4- TotalSum_
+
+This is a valid name because we use the case sensitive and the sign _ is valid 
+
+5. 12Byby
+
+This is an invalid name because starts with a digit and this is wrong
+
+6. num1
+
+This is a valid name because we use letters and number
+
+7. super$$
+
+This is a valid name bcause we use letters and the sign $$ is valid
+
+8. Object
+
+This is a invalid name because we can't use reserved words 
+
+9. StringNew
+
+This is a valid name because we use CamelCase
+
+10. $rankInClass
+
+This is a valid name because we can start with the sign $ and has a CamelCase
+
+
+*/
+
